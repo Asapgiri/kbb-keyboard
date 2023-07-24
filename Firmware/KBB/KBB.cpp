76b09dd9809fad1f5b1cbc7a5dccdc42121c2ed8
Asapgiri <razzero29@gmail.com>
2023-07-24 15:56:35 +0200

    function keys

--- Firmware/KBB/KBB.cpp
+++ Firmware/KBB/KBB.cpp
@@ -2,89 +2,106 @@
 #include <Arduino.h>
 #include <Keyboard.h>
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Private fields
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static bool win_locked = false;
+
+static void key_win_lock(void) {
+  win_locked = !win_locked;
+  digitalWrite(PIN_LED_WINLOCK, win_locked);
+}
+
+
 static struct char_holder Layout[NUMBER_OF_SEGS][KEYS_IN_SEGS] = {
   {
-    { def: KEY_ESC,           fn: '`',  fn_press: NULL, fn_release: NULL },
-    { def: 'w',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '4',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '5',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '7',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '9',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '-',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_BACKSPACE,     fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_ESC,           fn: '`',                fn_press: NULL, fn_release: NULL },
+    { def: 'w',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '4',               fn: KEY_F4,             fn_press: NULL, fn_release: NULL },
+    { def: '5',               fn: KEY_F5,             fn_press: NULL, fn_release: NULL },
+    { def: '7',               fn: KEY_F7,             fn_press: NULL, fn_release: NULL },
+    { def: '9',               fn: KEY_F9,             fn_press: NULL, fn_release: NULL },
+    { def: '-',               fn: KEY_F11,            fn_press: NULL, fn_release: NULL },
+    { def: KEY_BACKSPACE,     fn: NULL,               fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: '1',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '3',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'r',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '6',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '8',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '0',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '=',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_PRINT_SCREEN,  fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: '1',               fn: KEY_F1,             fn_press: NULL, fn_release: NULL },
+    { def: '3',               fn: KEY_F3,             fn_press: NULL, fn_release: NULL },
+    { def: 'r',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '6',               fn: KEY_F6,             fn_press: NULL, fn_release: NULL },
+    { def: '8',               fn: KEY_F8,             fn_press: NULL, fn_release: NULL },
+    { def: '0',               fn: KEY_F10,            fn_press: NULL, fn_release: NULL },
+    { def: '=',               fn: KEY_F12,            fn_press: NULL, fn_release: NULL },
+    { def: KEY_INSERT,        fn: KEY_PRINT_SCREEN,   fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: 'q',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 's',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'f',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'y',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'i',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'p',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: ']',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_DELETE,        fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: 'q',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 's',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'f',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'y',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'i',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'p',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: ']',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: KEY_END,           fn: NULL,               fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: KEY_TAB,           fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '2',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'e',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 't',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'u',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'o',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '[',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '\\',              fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_TAB,           fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '2',               fn: KEY_F2,             fn_press: NULL, fn_release: NULL },
+    { def: 'e',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 't',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'u',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'o',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '[',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '\\',              fn: NULL,               fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: 91,                fn: NULL, fn_press: NULL, fn_release: NULL }, // Left WIN
-    { def: 'x',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'v',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'h',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'k',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: ';',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_RIGHT_SHIFT,   fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_HOME,          fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_LEFT_WIN,      fn: NULL,               fn_press: NULL, fn_release: NULL }, // Left WIN
+    { def: 'x',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'v',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'h',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'k',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: ';',               fn: KEY_MUSIC_PREV,     fn_press: NULL, fn_release: NULL },
+    { def: KEY_RIGHT_SHIFT,   fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: KEY_DELETE,        fn: NULL,               fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: KEY_CAPS_LOCK,     fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'a',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'd',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'g',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'j',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'l',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '\'',              fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_RETURN,        fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_CAPS_LOCK,     fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'a',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'd',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'g',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'j',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'l',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '\'',              fn: KEY_MUSIC_NEXT,     fn_press: NULL, fn_release: NULL },
+    { def: KEY_RETURN,        fn: NULL,               fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: KEY_LEFT_CTRL,     fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_LEFT_ALT,      fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: ' ',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'n',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: ',',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_RIGHT_ALT,     fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: FUNCTION_KEY,      fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: KEY_END,           fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_LEFT_CTRL,     fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: KEY_LEFT_ALT,      fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: ' ',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'n',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: ',',               fn: KEY_VOLUME_DOWN,    fn_press: NULL, fn_release: NULL },
+    { def: KEY_RIGHT_ALT,     fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: FUNCTION_KEY,      fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: KEY_HOME,          fn: KEY_LEFT_SLEEP,     fn_press: NULL, fn_release: NULL }
   },
   {
-    { def: KEY_LEFT_SHIFT,    fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'z',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'c',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'b',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: 'm',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '.',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: '/',               fn: NULL, fn_press: NULL, fn_release: NULL },
-    { def: NULL,              fn: NULL, fn_press: NULL, fn_release: NULL }
+    { def: KEY_LEFT_SHIFT,    fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'z',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'c',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'b',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: 'm',               fn: NULL,               fn_press: NULL, fn_release: NULL },
+    { def: '.',               fn: KEY_VOLUME_UP,      fn_press: NULL, fn_release: NULL },
+    { def: '/',               fn: KEY_MUSIC_PLAY,     fn_press: NULL, fn_release: NULL },
+    { def: NULL,              fn: NULL,               fn_press: NULL, fn_release: NULL }
   }
   
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Public fields
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 KBB::KBB(){
   fn_pressed = false;
@@ -103,42 +120,46 @@ KBB::~KBB(){
 }
 
 void KBB::begin(){
-  pinMode(ADDR_A0, OUTPUT);
-  pinMode(ADDR_A1, OUTPUT);
-  pinMode(ADDR_A2, OUTPUT);
+  pinMode(PIN_ADDR_A0, OUTPUT);
+  pinMode(PIN_ADDR_A1, OUTPUT);
+  pinMode(PIN_ADDR_A2, OUTPUT);
 
-  pinMode(SEG0, INPUT);
-  pinMode(SEG1, INPUT);
-  pinMode(SEG2, INPUT);
-  pinMode(SEG3, INPUT);
-  pinMode(SEG4, INPUT);
-  pinMode(SEG5, INPUT);
-  pinMode(SEG6, INPUT);
-  pinMode(SEG7, INPUT);
+  pinMode(PIN_LED0, OUTPUT)
+  pinMode(PIN_LED1, OUTPUT)
+  pinMode(PIN_LED2, OUTPUT)
 
-  pinMode(ARROW_UP,    INPUT_PULLUP);
-  pinMode(ARROW_DOWN,  INPUT_PULLUP);
-  pinMode(ARROW_LEFT,  INPUT_PULLUP);
-  pinMode(ARROW_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_SEG0, INPUT);
+  pinMode(PIN_SEG1, INPUT);
+  pinMode(PIN_SEG2, INPUT);
+  pinMode(PIN_SEG3, INPUT);
+  pinMode(PIN_SEG4, INPUT);
+  pinMode(PIN_SEG5, INPUT);
+  pinMode(PIN_SEG6, INPUT);
+  pinMode(PIN_SEG7, INPUT);
+
+  pinMode(PIN_ARROW_UP,    INPUT_PULLUP);
+  pinMode(PIN_ARROW_DOWN,  INPUT_PULLUP);
+  pinMode(PIN_ARROW_LEFT,  INPUT_PULLUP);
+  pinMode(PIN_ARROW_RIGHT, INPUT_PULLUP);
 }
 
 void KBB::ChangeSegment(unsigned char seg){
-  digitalWrite(ADDR_A0, seg & 0x1);
-  digitalWrite(ADDR_A1, seg & 0x2);
-  digitalWrite(ADDR_A2, seg & 0x4);
+  digitalWrite(PIN_ADDR_A0, seg & 0x1);
+  digitalWrite(PIN_ADDR_A1, seg & 0x2);
+  digitalWrite(PIN_ADDR_A2, seg & 0x4);
   delayMicroseconds(50);
 }
 
 void KBB::RefreshActualKeyMap(unsigned char seg){
   /*The function refresh a segment from the keymap*/
-  ActualKeyMap[seg][0] = digitalRead(SEG0) == 0;
-  ActualKeyMap[seg][1] = digitalRead(SEG1) == 0;
-  ActualKeyMap[seg][2] = digitalRead(SEG2) == 0;
-  ActualKeyMap[seg][3] = digitalRead(SEG3) == 0;
-  ActualKeyMap[seg][4] = digitalRead(SEG4) == 0;
-  ActualKeyMap[seg][5] = digitalRead(SEG5) == 0;
-  ActualKeyMap[seg][6] = digitalRead(SEG6) == 0;
-  ActualKeyMap[seg][7] = digitalRead(SEG7) == 0;
+  ActualKeyMap[seg][0] = digitalRead(PIN_SEG0) == 0;
+  ActualKeyMap[seg][1] = digitalRead(PIN_SEG1) == 0;
+  ActualKeyMap[seg][2] = digitalRead(PIN_SEG2) == 0;
+  ActualKeyMap[seg][3] = digitalRead(PIN_SEG3) == 0;
+  ActualKeyMap[seg][4] = digitalRead(PIN_SEG4) == 0;
+  ActualKeyMap[seg][5] = digitalRead(PIN_SEG5) == 0;
+  ActualKeyMap[seg][6] = digitalRead(PIN_SEG6) == 0;
+  ActualKeyMap[seg][7] = digitalRead(PIN_SEG7) == 0;
 }
 
 void KBB::CopyActualToLastSegment(unsigned char seg){
@@ -167,7 +188,7 @@ bool KBB::CompareActualAndLastKeys(unsigned char seg){
   while(index < KEYS_IN_SEGS){
     PressKeyMap[seg][index]=false;
     ReleaseKeyMap[seg][index]=false;
-    
+
     if(LastKeyMap[seg][index] != ActualKeyMap[seg][index]){
       if(ActualKeyMap[seg][index]){
         PressKeyMap[seg][index]=true;
@@ -208,6 +229,10 @@ void KBB::SendChangesToHost(unsigned char seg) {
       }
     }
     else {
+      if (win_locked && KEY_LEFT_WIN == Layout[seg][key].def) {
+        continue;
+      }
+
       if(PressKeyMap[seg][key]){
         Keyboard.press(Layout[seg][key].def);
       }
--- Firmware/KBB/KBB.h
+++ Firmware/KBB/KBB.h
@@ -1,29 +1,45 @@
 #ifndef _KBB_H_
 #define _KBB_H_
 
-#define ADDR_A0 7
-#define ADDR_A1 19
-#define ADDR_A2 18
+#define PIN_ADDR_A0 7
+#define PIN_ADDR_A1 19
+#define PIN_ADDR_A2 18
 
-#define SEG0 4
-#define SEG1 12
-#define SEG2 6
-#define SEG3 8
-#define SEG4 9
-#define SEG5 10
-#define SEG6 5
-#define SEG7 13
+#define PIN_SEG0 4
+#define PIN_SEG1 12
+#define PIN_SEG2 6
+#define PIN_SEG3 8
+#define PIN_SEG4 9
+#define PIN_SEG5 10
+#define PIN_SEG6 5
+#define PIN_SEG7 13
 
-#define ARROW_UP    22
-#define ARROW_DOWN  21
-#define ARROW_LEFT  23
-#define ARROW_RIGHT 20
+#define PIN_ARROW_UP    22
+#define PIN_ARROW_DOWN  21
+#define PIN_ARROW_LEFT  23
+#define PIN_ARROW_RIGHT 20
+
+#define PIN_LED0          30
+#define PIN_LED1          1
+#define PIN_LED2          0
+#define PIN_LED_CAPSLOCK  PIN_LED0
+#define PIN_LED_WINLOCK   PIN_LED1
 
 
 #define NUMBER_OF_SEGS  8
 #define KEYS_IN_SEGS    8
 
+#define KEY_LEFT_WIN    0x5B
+#define KEY_LEFT_SLEEP  0x5F
+#define KEY_VOLUME_UP   0xAF
+#define KEY_VOLUME_DOWN 0xAE
+#define KEY_MUSIC_PLAY  0xB3
+#define KEY_MUSIC_NEXT  0xB0
+#define KEY_MUSIC_PREV  0xB1
+
 #define FUNCTION_KEY    0x1
+
+
 
 struct char_holder {
   char def;
@@ -50,5 +66,6 @@ public:
   KBB();
   ~KBB();
 };
+
 
 #endif _KBB_H_
